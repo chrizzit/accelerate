6726c8dbecb139642b9512ea9afbc7956655c884
Chrizzit <christopher.valenciano@gmail.com>
2017-07-26 11:46:26 -0400

    Added styles to Case Studies page

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -12,8 +12,10 @@ get_header(); ?>
 	<div id="primary" class="site-content">
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
-        <h1>This is a Case Study page</h1>
+        <!--<h1>This is a Case Study page</h1>-->
+        <article class="case-study">
 				<?php the_content(); ?>
+      </article>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
